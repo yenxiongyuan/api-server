4ee1a42c2b55cf3b172d1d30d3747d8d143590e5
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-03-05 05:50:01 +0000

    worked on lab 4

---
+++ src/models/collection.js
@@ -0,0 +1,57 @@
+'use strict';
+
+class Collection {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(json) {
+    try {
+      const record = await this.model.create(json);
+      return record;
+    } catch (e) {
+      console.error('error in the create collection interface');
+      return e;
+    }
+  }
+
+  async read(id = null) {
+    try {
+      if (!id) {
+        // get all
+        const records = await this.model.findAll();
+        return records;
+      } else {
+        // get by id
+        const singleRecord = await this.model.findByPk(id);
+        return singleRecord;
+      }
+    } catch (e) {
+      console.error('error in the read collection interface');
+      return e;
+    }
+  }
+
+  async update(json, id) {
+    try {
+      const updateRecord = await this.model.update(json, {where: {id}});
+      return updateRecord;
+    } catch (e) {
+      console.error('error in the update collection interface');
+      return e;
+    }
+  }
+
+  async delete(id) {
+    try {
+      await this.model.destroy({where : {id}});
+      return 'record deleted';
+    } catch (e) {
+      console.error('error in the delete collection interface');
+      return e;
+    }
+  }
+
+}
+
+module.exports = Collection;
